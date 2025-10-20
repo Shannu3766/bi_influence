ff16d7aa23c753722cab3e06f658d42009a9ea6d
Shanmukha Srinivas Ch <shanmukhasrinivas82@gmail.com>
2025-10-20 21:23:03 +0530

    Update fine_tune_lora import to fine_tune_lora_dynamic in __init__.py

--- adalora_bi/__init__.py
+++ adalora_bi/__init__.py
@@ -1,12 +1,12 @@
 from .allocation import bi_allocate_ranks
 from .importance import compute_bi_importance_from_dataloader
 from .lora_injector import LoRALinear, inject_adaptive_lora
-from .trainer import fine_tune_lora
+from .trainer import fine_tune_lora_dynamic
 
 __all__ = [
     "bi_allocate_ranks",
     "compute_bi_importance_from_dataloader",
     "LoRALinear",
     "inject_adaptive_lora",
-    "fine_tune_lora",
+    "fine_tune_lora_dynamic",
 ]
